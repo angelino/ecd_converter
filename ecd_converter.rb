4e4ce33476dbe65973e0bc66c5005f259452a9c3
Lucas Angelino <lucas.angelino@gmail.com>
2015-09-10 22:37:16 -0300

    Initial Prototype

--- ecd_converter.rb
+++ ecd_converter.rb
@@ -1,13 +1,36 @@
 puts "ECD Converter"
 
-contents = File.read('ecd.txt')
+PATTERN = /\|I051\|\w\|\|\d+\|/
+
+require 'csv'
+
+filename = 'tabela_ecd.csv'
+data = CSV.open(filename, 'r', encoding: 'utf-8', col_sep: ','
+  # , headers: true, header_converters: :symbol
+  )
+
+COD_TABLE = Hash[data.entries]
+
+def find(cod)
+  COD_TABLE[cod]
+end
+
+content = File.read('ecd.txt')
 puts "Arquivo lido"
 
-result = contents.gsub(//,'')
+replaceable_lines = content.scan(PATTERN)
+
+replaceable_lines.uniq.each do |old_line|
+  cod_ref = old_line.split('|').last
+  new_cod = find(cod_ref)
+  new_line = old_line.sub(cod_ref, new_cod) if new_cod
+
+  content.gsub!(old_line, new_line) if new_line
+end
 puts "Arquivo alterado"
 
-File.write('ecd.txt', 'w') do |f|
-  f.write(result)
+File.open('ecd.txt', 'w') do |f|
+  f.write(content)
 end
 puts "Arquivo gravado"
 
